0b2fa1579e7e868555f4f6e724dcb55a925bcf96
Pau Tallada Crespí <pau.tallada@gmail.com>
2022-02-22 15:12:02 +0100

    Cleaned pix2ang and pix2vec.

--- src/main/java/es/pic/hadoop/udf/healpix/UDFPix2Ang.java
+++ src/main/java/es/pic/hadoop/udf/healpix/UDFPix2Ang.java
@@ -1,8 +1,6 @@
-package es.pic.astro.hive_udf;
+package es.pic.hadoop.udf.healpix;
 
-import java.lang.Math;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
@@ -16,119 +14,119 @@ import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorConverters.C
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
 import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
-import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.BooleanWritable;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.ByteWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
+
 import healpix.essentials.HealpixProc;
 import healpix.essentials.Pointing;
 
+// @formatter:off
 @Description(
-    name="pix2ang",
-    value="_FUNC_(order, pix, nest, lonlat) - Returns a struct with the angles of the pix",
-    extended="SELECT _FUNC_(3, 646, true, true) FROM foo LIMIT 1;"
+    name = "pix2ang",
+    value = "_FUNC_(order:tinyint, ipix:bigint, [nest:bool=False, [lonlat:bool=False]]) -> array<float>(theta/dec, phi/ra)",
+    extended = "Return the angular coordinates corresponding to this pixel."
 )
 @UDFType(
     deterministic = true,
     stateful = false
 )
+// @formatter:on
 public class UDFPix2Ang extends GenericUDF {
 
-    private final Object[] result = new Object[2];
-    private final DoubleWritable phiWritable = new DoubleWritable();
-    private final DoubleWritable thetaWritable = new DoubleWritable();
+    Converter orderConverter;
+    Converter ipixConverter;
+    Converter nestConverter;
+    Converter lonlatConverter;
 
-    private Converter[] intconverter = new Converter[1];
-    private Converter[] longconverter = new Converter[1];
-    private Converter[] booleanconverter = new Converter[2];
-    
+    final static ObjectInspector byteOI = PrimitiveObjectInspectorFactory
+            .getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.BYTE);
+    final static ObjectInspector longOI = PrimitiveObjectInspectorFactory
+            .getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.LONG);
+    final static ObjectInspector boolOI = PrimitiveObjectInspectorFactory
+            .getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.BOOLEAN);
+    final static ObjectInspector doubleOI = PrimitiveObjectInspectorFactory
+            .getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.DOUBLE);
+
+    ByteWritable orderArg = new ByteWritable();
+    LongWritable ipixArg = new LongWritable();
+    BooleanWritable nestArg = new BooleanWritable();
+    BooleanWritable lonlatArg = new BooleanWritable();
+
+    byte order;
+    long ipix;
+    boolean nest;
+    boolean lonlat;
+
+    DoubleWritable theta = new DoubleWritable();
+    DoubleWritable phi = new DoubleWritable();
+    DoubleWritable ra = new DoubleWritable();
+    DoubleWritable dec = new DoubleWritable();
+
     @Override
     public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
-        if (arguments.length<2|| arguments.length>4) {
-            throw new UDFArgumentLengthException("pix2ang() takes at least 2 arguments, no more than 4: order, pix, nest, lonlat");
-        }
-        List<String> fieldNames = new ArrayList<String>();
-        List<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>();
-
-        ObjectInspector intOI = PrimitiveObjectInspectorFactory.getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.INT);
-        ObjectInspector longOI = PrimitiveObjectInspectorFactory.getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.LONG);
-        intconverter[0] = (Converter) ObjectInspectorConverters.getConverter(arguments[0], intOI);
-        longconverter[0] = (Converter) ObjectInspectorConverters.getConverter(arguments[1], longOI);
-        if (arguments.length == 3) {
-            ObjectInspector boolOI = PrimitiveObjectInspectorFactory.getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.BOOLEAN);
-            booleanconverter[0] = (Converter) ObjectInspectorConverters.getConverter(arguments[2], boolOI);
-        } else if (arguments.length == 4) {
-            ObjectInspector boolOI = PrimitiveObjectInspectorFactory.getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.BOOLEAN);
-            booleanconverter[0] = (Converter) ObjectInspectorConverters.getConverter(arguments[2], boolOI);
-            booleanconverter[1] = (Converter) ObjectInspectorConverters.getConverter(arguments[3], boolOI);
+        if (arguments.length < 2 || arguments.length > 4) {
+            throw new UDFArgumentLengthException(
+                    "This function takes at least 2 arguments, no more than 4: order, ipix, nest, lonlat");
         }
 
-        fieldNames.add("theta_lat");
-        fieldNames.add("phi_lon");
-        fieldOIs.add(PrimitiveObjectInspectorFactory.writableDoubleObjectInspector);
-        fieldOIs.add(PrimitiveObjectInspectorFactory.writableDoubleObjectInspector);
-        result[1] = phiWritable;
-        result[0] = thetaWritable;
+        orderConverter = ObjectInspectorConverters.getConverter(arguments[0], byteOI);
+        ipixConverter = ObjectInspectorConverters.getConverter(arguments[1], longOI);
 
-        return ObjectInspectorFactory.getStandardStructObjectInspector(fieldNames, fieldOIs);
+        if (arguments.length >= 3) {
+            nestConverter = ObjectInspectorConverters.getConverter(arguments[2], boolOI);
+        }
+        if (arguments.length == 4) {
+            lonlatConverter = ObjectInspectorConverters.getConverter(arguments[3], boolOI);
+        }
+
+        return ObjectInspectorFactory.getStandardListObjectInspector(doubleOI);
     }
-
 
     @Override
     public Object evaluate(DeferredObject[] arguments) throws HiveException {
-        IntWritable argsw1 = new IntWritable();
-        LongWritable argsw2 = new LongWritable();
+        orderArg = (ByteWritable) orderConverter.convert(arguments[0].get());
+        ipixArg = (LongWritable) ipixConverter.convert(arguments[1].get());
 
-        BooleanWritable argsw3 = new BooleanWritable(false);
-        BooleanWritable argsw4 = new BooleanWritable(false);
-
-        argsw1 = (IntWritable) intconverter[0].convert(arguments[0].get());
-        argsw2 = (LongWritable) longconverter[0].convert(arguments[1].get());
-
-        if (arguments.length == 3) {
-            argsw3 = (BooleanWritable) booleanconverter[0].convert(arguments[2].get());
-        } else if (arguments.length == 4) {
-            argsw3 = (BooleanWritable) booleanconverter[0].convert(arguments[2].get());
-            argsw4 = (BooleanWritable) booleanconverter[1].convert(arguments[3].get());
+        if (arguments.length >= 3) {
+            nestArg = (BooleanWritable) nestConverter.convert(arguments[2].get());
         }
-        if (argsw1 == null || argsw2 == null) {
-            result[0] = null;
-            result[1] = null;
-            return result;
+        if (arguments.length == 4) {
+            lonlatArg = (BooleanWritable) lonlatConverter.convert(arguments[3].get());
+        }
+        if (orderArg == null || ipixArg == null) {
+            return null;
         }
 
-        int order = argsw1.get();
-        long pix = argsw2.get();
-        boolean nest = argsw3.get();
-        boolean lonlat = argsw4.get();
-        Pointing res = new Pointing();
+        order = orderArg.get();
+        ipix = ipixArg.get();
+        nest = nestArg.get();
+        lonlat = lonlatArg.get();
 
+        Pointing pt = new Pointing();
         try {
-            if (nest == true) {     
-                res = HealpixProc.pix2angNest(order, pix); 
+            if (nest == true) {
+                pt = HealpixProc.pix2angNest(order, ipix);
             } else {
-                res = HealpixProc.pix2angRing(order, pix);
+                pt = HealpixProc.pix2angRing(order, ipix);
             }
-        } catch(Exception e){}
-        
+        } catch (Exception e) {
+        }
+
         if (lonlat) {
-            thetaWritable.set(90 - res.theta * 180 /  Math.PI);
-            phiWritable.set(res.phi * 180 / Math.PI);
-	    result[0] = phiWritable;
-            result[1] = thetaWritable;
+            dec.set(90 - pt.theta * 180 / Math.PI);
+            ra.set(pt.phi * 180 / Math.PI);
+            return Arrays.asList(dec, ra);
         } else {
-            thetaWritable.set(res.theta);
-            phiWritable.set(res.phi);
-            result[0] = thetaWritable;
-            result[1] = phiWritable;
-	}
-
-        return result;
+            theta.set(pt.theta);
+            phi.set(pt.phi);
+            return Arrays.asList(theta, phi);
+        }
     }
-
 
     @Override
     public String getDisplayString(String[] arg0) {
-        return "pix2ang(order, hpix, nest, lonlat)";
+        return String.format("arguments (%d, %d, %b, %b)", order, ipix, nest, lonlat);
     }
 }
--- src/main/java/es/pic/hadoop/udf/healpix/UDFPix2Vec.java
+++ src/main/java/es/pic/hadoop/udf/healpix/UDFPix2Vec.java
@@ -1,7 +1,6 @@
-package es.pic.astro.hive_udf;
+package es.pic.hadoop.udf.healpix;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
@@ -15,111 +14,104 @@ import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorConverters.C
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
 import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
-import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.BooleanWritable;
+import org.apache.hadoop.io.ByteWritable;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+
 import healpix.essentials.HealpixProc;
 import healpix.essentials.Vec3;
 
+// @formatter:off
 @Description(
-    name="pix2vec",
-    value="_FUNC_(order, pix, nest) - Returns a struct with the vector of the pix",
-    extended="SELECT _FUNC_(3, 646, false) FROM foo LIMIT 1;"
+    name = "pix2vec",
+    value = "_FUNC_(order:tinyint, ipix:bigint, [nest:bool=False]) -> array<float>(x, y, z)",
+    extended = "Return the 3D position vector corresponding to this pixel."
 )
 @UDFType(
     deterministic = true,
     stateful = false
 )
+// @formatter:on
 public class UDFPix2Vec extends GenericUDF {
 
-    private final Object[] result = new Object[3];
-    private final DoubleWritable xWritable = new DoubleWritable();
-    private final DoubleWritable yWritable = new DoubleWritable();
-    private final DoubleWritable zWritable = new DoubleWritable();    
+    Converter orderConverter;
+    Converter ipixConverter;
+    Converter nestConverter;
 
-    private Converter[] intconverter = new Converter[1];
-    private Converter[] longconverter = new Converter[1];
-    private Converter[] booleanconverter = new Converter[1];
-    
+    final static ObjectInspector byteOI = PrimitiveObjectInspectorFactory
+            .getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.BYTE);
+    final static ObjectInspector longOI = PrimitiveObjectInspectorFactory
+            .getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.LONG);
+    final static ObjectInspector boolOI = PrimitiveObjectInspectorFactory
+            .getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.BOOLEAN);
+    final static ObjectInspector doubleOI = PrimitiveObjectInspectorFactory
+            .getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.DOUBLE);
+
+    ByteWritable orderArg = new ByteWritable();
+    LongWritable ipixArg = new LongWritable();
+    BooleanWritable nestArg = new BooleanWritable();
+
+    byte order;
+    long ipix;
+    boolean nest;
+
+    DoubleWritable x = new DoubleWritable();
+    DoubleWritable y = new DoubleWritable();
+    DoubleWritable z = new DoubleWritable();
+
     @Override
     public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
         if (arguments.length < 2 || arguments.length > 3) {
-            throw new UDFArgumentLengthException("pix2vec() takes at least 2 arguments, no more than 3: order, pix, nest");
+            throw new UDFArgumentLengthException(
+                    "This function takes at least 2 arguments, no more than 3: order, ipix, nest");
         }
 
-        List<String> fieldNames = new ArrayList<String>();
-        List<ObjectInspector> fieldOIs = new ArrayList<ObjectInspector>();
-
-        ObjectInspector intOI = PrimitiveObjectInspectorFactory.getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.INT);
-        ObjectInspector longOI = PrimitiveObjectInspectorFactory.getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.LONG);
-
-        intconverter[0] = (Converter) ObjectInspectorConverters.getConverter(arguments[0], intOI);
-        longconverter[0] = (Converter) ObjectInspectorConverters.getConverter(arguments[1], longOI);
+        orderConverter = ObjectInspectorConverters.getConverter(arguments[0], byteOI);
+        ipixConverter = ObjectInspectorConverters.getConverter(arguments[1], longOI);
 
         if (arguments.length == 3) {
-            ObjectInspector boolOI = PrimitiveObjectInspectorFactory.getPrimitiveWritableObjectInspector(PrimitiveObjectInspector.PrimitiveCategory.BOOLEAN);
-            booleanconverter[0] = (Converter) ObjectInspectorConverters.getConverter(arguments[2], boolOI);
+            nestConverter = ObjectInspectorConverters.getConverter(arguments[2], boolOI);
         }
 
-        fieldNames.add("x");
-        fieldNames.add("y");
-        fieldNames.add("z");
-        fieldOIs.add(PrimitiveObjectInspectorFactory.writableDoubleObjectInspector);
-        fieldOIs.add(PrimitiveObjectInspectorFactory.writableDoubleObjectInspector);
-        fieldOIs.add(PrimitiveObjectInspectorFactory.writableDoubleObjectInspector);
-        result[0] = xWritable;
-        result[1] = yWritable;
-        result[2] = zWritable;
-
-        return ObjectInspectorFactory.getStandardStructObjectInspector(fieldNames, fieldOIs);
+        return ObjectInspectorFactory.getStandardListObjectInspector(doubleOI);
     }
-
 
     @Override
     public Object evaluate(DeferredObject[] arguments) throws HiveException {
-        IntWritable argsw1 = new IntWritable();
-        LongWritable argsw2 = new LongWritable();
-        BooleanWritable argsw3 = new BooleanWritable(false);
-
-        argsw1 = (IntWritable) intconverter[0].convert(arguments[0].get());
-        argsw2 = (LongWritable) longconverter[0].convert(arguments[1].get());
+        orderArg = (ByteWritable) orderConverter.convert(arguments[0].get());
+        ipixArg = (LongWritable) ipixConverter.convert(arguments[1].get());
 
         if (arguments.length == 3) {
-            argsw3 = (BooleanWritable) booleanconverter[0].convert(arguments[2].get());
+            nestArg = (BooleanWritable) nestConverter.convert(arguments[2].get());
         }
-        if (argsw1 == null || argsw2 == null){
-            result[0] = null;
-            result[1] = null;
-            return result;
+        if (orderArg == null || ipixArg == null) {
+            return null;
         }
 
-        int order = argsw1.get();
-        long pix = argsw2.get();
-        boolean nest = argsw3.get();
-        Vec3 res = new Vec3();
+        order = orderArg.get();
+        ipix = ipixArg.get();
+        nest = nestArg.get();
 
-        try{     
+        Vec3 vec = new Vec3();
+        try {
             if (nest == true) {
-                res = HealpixProc.pix2vecNest(order, pix);
+                vec = HealpixProc.pix2vecNest(order, ipix);
             } else {
-                res = HealpixProc.pix2vecRing(order, pix);
-            } 
-        }catch(Exception e){}
-        
-        xWritable.set(res.x);
-        yWritable.set(res.y);
-        zWritable.set(res.z);
- 
-        result[0] = xWritable;
-        result[1] = yWritable;
-        result[2] = zWritable;
+                vec = HealpixProc.pix2vecRing(order, ipix);
+            }
+        } catch (Exception e) {
+        }
 
-        return result;
+        x.set(vec.x);
+        y.set(vec.y);
+        z.set(vec.z);
+
+        return Arrays.asList(x, y, z);
     }
 
     @Override
     public String getDisplayString(String[] arg0) {
-        return "pix2vec(order, hpix, nest)";
+        return String.format("arguments (%d, %d, %b)", order, ipix, nest);
     }
 }
